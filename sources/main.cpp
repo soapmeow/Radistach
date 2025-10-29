480fac750f5873eb58b73d1c8076ace9ac1833a4
Shuvia <nikolai.romanov013@gmail.com>
2025-10-29 20:46:34 +0300

    I fuckin' hate opengl

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,6 +3,27 @@
 #include <iostream>
 
 
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0f"
+
 int main(void)
 {
     GLFWwindow* window;
