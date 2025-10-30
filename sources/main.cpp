6f665ca5e7a212b7472c6c6aaa8c3011689b2bd9
Shuvia <nikolai.romanov013@gmail.com>
2025-10-30 14:10:31 +0300

    Return back what was here

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,6 +2,23 @@
 #include "geometry.h"
 
 int main(int argc, char* argv[]){
+	Vec3d pointA(0,0,0);
+	Vec2d pointB(0,0);
+
+	std::cout << "Enter 3d point!: ";
+	std::cin >> pointA.x >> pointA.y >> pointA.z;
+	std::cout << "success!\n";
+
+
+	std::cout << "Enter 2d point!: ";
+	std::cin >> pointB.x >> pointB.y;
+	std::cout << "success!\n";
 	
+	std::cout << pointA.x << " " << pointA.y << " " << pointA.z << std::endl;
+	std::cout << pointB.x << " " << pointB.y << std::endl;
+
+	char temp;
+	std::cin.get(temp);
+
 	return EXIT_SUCCESS;
 }
